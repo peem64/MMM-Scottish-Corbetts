f652be50e343b5632ff0237d14bc746a630c9ee2
peem64 <jim@arbroath-walking.football>
2025-08-03 23:29:43 +0100

    Actually fix the migration by removing ID from INSERT

---
+++ supabase/migrations/20250803222939_royal_breeze.sql
@@ -0,0 +1,6 @@
+@@ .. @@
+ */
+ 
+-INSERT INTO corbetts (id, name, location, region, height, prominence, classification, estimated_time, description, difficulty_rating, popular_routes, best_seasons, grid_reference, nearest_town, parking_info, image_filename) VALUES
++INSERT INTO corbetts (name, location, region, height, prominence, classification, estimated_time, description, difficulty_rating, popular_routes, best_seasons, grid_reference, nearest_town, parking_info, image_filename) VALUES
+ (1, 'Ainshval', 'Isle of Rum', 'Isle of Rum', 781, 180, 'Corbett', '5-6 hours', 'A dramatic peak on the Isle of Rum with spectacular views across the Inner Hebrides.', 4, ARRAY['Kinloch approach', 'Coastal path'], ARRAY['Spring', 'Summer', 'Autumn'], 'NM373945', 'Kinloch', 'Kinloch village', 'ainshval.jpg'),
