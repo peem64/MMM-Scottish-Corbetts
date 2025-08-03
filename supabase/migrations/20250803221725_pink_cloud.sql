0903f2ad6dfabb802227ede761eee1260863eee8
peem64 <jim@arbroath-walking.football>
2025-08-03 23:17:27 +0100

    Remove ID column from INSERT statement

---
+++ supabase/migrations/20250803221725_pink_cloud.sql
@@ -0,0 +1,5 @@
+@@ .. @@
+ */
+
+-INSERT INTO corbetts (id, name, location, region, height, prominence, classification, estimated_time, description, difficulty_rating, popular_routes, best_seasons, grid_reference, nearest_town, parking_info, image_filename) VALUES
++INSERT INTO corbetts (name, location, region, height, prominence, classification, estimated_time, description, difficulty_rating, popular_routes, best_seasons, grid_reference, nearest_town, parking_info, image_filename) VALUES
